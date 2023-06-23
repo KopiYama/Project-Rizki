83e9dc4cc67b2e021ce7ff258157ff5f218bf267
Mochamad Rizky Andika <mrrizkya46@gmail.com>
2023-06-23 18:18:30 +0700

    Bebas

--- Assets/Code/Ripan/ButtonManager.cs
+++ Assets/Code/Ripan/ButtonManager.cs
@@ -2,19 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class ButtonManager : MonoBehaviour
 {
+    private int acuan = 0;
+    private int id_button = 0;
+    private int hitung = 0;
+
     public GameObject workspace;
     public List<GameObject> buttons;
+    public List<Button> onOff;
     public float offsetX;
+
+    public GameObject[] bottoms;
+
+    public GameObject endButton;
 
     private List<GameObject> instantiatedButtons = new List<GameObject>();
     private List<string> labelPuzzle = new List<string>();
 
+    private string path;
+
     void Start()
     {
-        buttons.Clear();
+        for(int i=0; i< bottoms.Length; i++)
+        {
+            bottoms[i].SetActive(false);
+        }
+
+        bottoms[0].SetActive(true);
     }
 
     private void PositionButtons()
@@ -23,8 +40,8 @@ public class ButtonManager : MonoBehaviour
         {
             RectTransform rectTransform = instantiatedButtons[i].GetComponent<RectTransform>();
             float buttonWidth = rectTransform.rect.width;
-            float xPos = -627 + (buttonWidth * i) + (offsetX * i);
-            rectTransform.localPosition = new Vector3(xPos, 0, 0);
+            float xPos = -487 + (buttonWidth * i) + (offsetX * i);
+            rectTransform.localPosition = new Vector3(xPos, 92, 0);
         }
     }
 
@@ -32,6 +49,29 @@ public class ButtonManager : MonoBehaviour
     {
         buttons.Clear();
         buttons.Add(id);
+
+        acuan++;
+
+        if(acuan % 2 != 0)
+        {
+            bottoms[0].SetActive(false);
+
+            if(id_button == 0)
+            {
+                bottoms[1].SetActive(true);
+            }
+        }
+
+        else
+        {
+            hitung++;
+            for(int i=0; i< bottoms.Length; i++)
+            {
+                bottoms[i].SetActive(false);
+            }
+
+            bottoms[0].SetActive(true);
+        }
 
         // Instantiate buttons as children of the workspace and disable their Button components
         for (int i = 0; i < buttons.Count; i++)
@@ -45,6 +85,11 @@ public class ButtonManager : MonoBehaviour
         PositionButtons();
     }
 
+    public void GetIdButton(int id)
+    {
+        id_button = id;
+    }
+
     public void ResetLabel()
     {
         buttons.Clear();
@@ -56,6 +101,11 @@ public class ButtonManager : MonoBehaviour
             Destroy(buttonInstance);
         }
         instantiatedButtons.Clear();
+
+        for(int i=0; i<onOff.Count; i++)
+        {
+            onOff[i].interactable = true;
+        }
     }
 
     public void LabelButton(string label)
@@ -65,9 +115,40 @@ public class ButtonManager : MonoBehaviour
 
     public void PrintLabel()
     {
+        string nilai = "";
+        labelPuzzle.Add("S");
+        GameObject buttonInstance = Instantiate(endButton, workspace.transform);
+        instantiatedButtons.Add(buttonInstance);
+
+        PositionButtons();
+
+        for(int i=0; i<onOff.Count; i++)
+        {
+            onOff[i].interactable = false;
+        }
+
+        path = Application.dataPath + "/MyTextFile.txt";
+
         foreach(var label in labelPuzzle)
         {
-            Debug.Log(label);
+            nilai += label;
         }
+
+        WriteText(nilai);
+    }
+
+    private void WriteText(string content)
+    {
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, false);
+        writer.WriteLine(content);
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        UnityEditor.AssetDatabase.ImportAsset(path); 
+
+        //Print the text from the file
+        string text = File.ReadAllText(path);
+        Debug.Log(text);
     }
 }
